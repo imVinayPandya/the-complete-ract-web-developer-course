934ace3781baaed1c6f3a70543f981bc4e5a5690
Vinay Pandya <005vinay@gmail.com>
2018-08-08 13:45:16 +0530

    27. Nesting Components

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,5 +1,18 @@
 // import React from 'react';
 
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    );
+  }
+}
+
 class Header extends React.Component {
   render() {
     return (
@@ -25,11 +38,19 @@ class Options extends React.Component {
   render() {
     return (
       <div>
-        <ul>
-          <li>First</li>
-          <li>Second</li>
-          <li>third</li>
-        </ul>
+        <Option />
+      </div>
+    );
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Option one</p>
+        <p>Option two</p>
+        <p>Option three</p>
       </div>
     );
   }
@@ -45,13 +66,4 @@ class AddOption extends React.Component {
   }
 }
 
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-  </div>
-);
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
