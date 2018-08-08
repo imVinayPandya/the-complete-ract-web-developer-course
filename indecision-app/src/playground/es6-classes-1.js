9616facaa047ea504507dcfacf44c3178a98be09
Vinay Pandya <005vinay@gmail.com>
2018-08-08 13:17:46 +0530

    25. ES6 Classes: Part II

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -5,7 +5,7 @@ class Person {
     this.age = age;
   }
 
-  getGretting() {
+  getGreeting() {
     return `Hi, I am ${this.name}!`;
   }
 
@@ -14,9 +14,46 @@ class Person {
   }
 }
 
-const me = new Person('Vinay pandya', 26);
-console.log(me.getDescription());
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
 
-const other = new Person();
-console.log(other.getDescription());
+  hasMajor() {
+    return !!this.major;
+  }
+
+  getDescription() {
+    let description = super.getDescription();
+    if (this.hasMajor()) {
+      description += ` Their Major is ${this.major}.`
+    }
+
+    return description;
+  }
+}
+
+class Traveler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+
+  getGreeting() {
+    let gretting = super.getGreeting();
+
+    if(this.homeLocation) {
+      gretting += ` I'm visiting from ${this.homeLocation}`;
+    }
+
+    return gretting;
+  }
+}
+
+const me = new Traveler('Vinay pandya', 26, 'Ahemdabad');
+console.log(me.getGreeting());
+
+const other = new Traveler(undefined, undefined, 'Nowhere');
+console.log(other.getGreeting());
 
