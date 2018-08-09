aca90015e3216eda0682ea7568cd770ff344f49c
Vinay Pandya <005vinay@gmail.com>
2018-08-09 15:53:10 +0530

    52. Default Exports

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,12 +1,16 @@
-// // import './utils';
+// import './utils';
 
-// import { square, add } from './utils';
+// import subtract, { square, add } from './utils';
 
 // console.log('app.js is running!');
 // console.log(square(4));
 // console.log(add(10, 20));
+// console.log(subtract(100, 19));
 
-import { canDrink, isAdult } from './person';
+
+
+import isSenior, { canDrink, isAdult } from './person';
 
 console.log("canDrink: ", canDrink(17));
 console.log("isAdult: ", isAdult(19));
+console.log("isSenior: ", isSenior(59));
--- indecision-app/src/person.js
+++ indecision-app/src/person.js
@@ -1,4 +1,6 @@
 const isAdult = (age) => age >= 18;
 const canDrink = (age) => age >= 18;
+const isSenior = (age) => age >= 60;
 
+export default isSenior;
 export { isAdult, canDrink };
