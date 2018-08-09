d805ef75951b253aea57a52bcf07487f3525ec13
Vinay Pandya <005vinay@gmail.com>
2018-08-09 16:04:25 +0530

    53. Importing npm Modules

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,16 +1,5 @@
-// import './utils';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// import subtract, { square, add } from './utils';
-
-// console.log('app.js is running!');
-// console.log(square(4));
-// console.log(add(10, 20));
-// console.log(subtract(100, 19));
-
-
-
-import isSenior, { canDrink, isAdult } from './person';
-
-console.log("canDrink: ", canDrink(17));
-console.log("isAdult: ", isAdult(19));
-console.log("isSenior: ", isSenior(59));
+const template = React.createElement('p', null, 'This is react')
+ReactDOM.render(template, document.getElementById('app'));
