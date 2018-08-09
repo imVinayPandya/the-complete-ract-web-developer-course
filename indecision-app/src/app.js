64eed44b6e725431b6da900624d1111d9bdfb01a
Vinay Pandya <005vinay@gmail.com>
2018-08-09 15:41:06 +0530

    51. ES6 import/export

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,1 +1,12 @@
-console.log('app.js is running!');
+// // import './utils';
+
+// import { square, add } from './utils';
+
+// console.log('app.js is running!');
+// console.log(square(4));
+// console.log(add(10, 20));
+
+import { canDrink, isAdult } from './person';
+
+console.log("canDrink: ", canDrink(17));
+console.log("isAdult: ", isAdult(19));
