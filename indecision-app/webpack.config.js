4976af3b5928ab4bbedefc67bfdfbb333ba4416b
Vinay Pandya <005vinay@gmail.com>
2018-08-10 13:53:11 +0530

    66. Reset That $#!*

--- indecision-app/webpack.config.js
+++ indecision-app/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     rules: [
       { loader: 'babel-loader', test: /\.js$/, exclude: /node_modules/ },
       {
-        test: /\.scss$/,
+        test: /\.s?css$/,
         use: [
           'style-loader',
           'css-loader',
