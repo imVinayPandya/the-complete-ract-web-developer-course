65fdbbb4b80c39ffbc09dca94386e576623444a0
Vinay Pandya <005vinay@gmail.com>
2018-08-10 15:24:03 +0530

    70. Styling Option Item

--- indecision-app/src/components/Option.js
+++ indecision-app/src/components/Option.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Option = (props) => {
-  const { optionText, handleDeleteOption } = props;
+  const { optionText, handleDeleteOption, count } = props;
   return (
-    <div>
-      {optionText}
+    <div className='option'>
+      <p className='option__text'>{count}. {optionText}</p>
+
       <button
         className='button button--link'
         onClick={(e) => {
--- indecision-app/src/components/Options.js
+++ indecision-app/src/components/Options.js
@@ -18,10 +18,11 @@ const Options = (props) => {
       </div>
       {options.length === 0 && <p className="widget__message">Please add an update to get started!</p>}
       {
-        options.map((option) => (
+        options.map((option, index) => (
           <Option
             key={option}
             optionText={option}
+            count={index + 1}
             handleDeleteOption={handleDeleteOption}
           />
         ))
