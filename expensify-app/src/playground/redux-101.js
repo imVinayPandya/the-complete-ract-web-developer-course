9ff7baef5e34fece76423b6c21d0b883167c09f9
Vinay Pandya <005vinay@gmail.com>
2018-08-12 00:37:39 +0530

    87. Subscribing and Dynamic Actions

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -3,12 +3,19 @@ import { create, createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
     case 'DECREMENT':
+      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
+      };
+    case 'SET':
+      const count = typeof action.count === 'number' ? action.count : 0;
+      return {
+        count: count
       };
     case 'RESET':
       return {
@@ -19,16 +26,19 @@ const store = createStore((state = { count: 0 }, action) => {
   }
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
 
 // increment the count
 store.dispatch({
-  type: 'INCREMENT'
+  type: 'INCREMENT',
+  incrementBy: 5
 });
 
 // decrement the count
 store.dispatch({
-  type: 'DECREMENT'
+  type: 'INCREMENT'
 });
 
 // reset the count
@@ -40,4 +50,12 @@ store.dispatch({
   type: 'DECREMENT'
 });
 
-console.log(store.getState());
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 10
+});
+
+store.dispatch({
+  type: 'SET',
+  count: 101
+});
