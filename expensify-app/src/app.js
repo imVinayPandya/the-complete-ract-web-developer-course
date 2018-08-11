c52dbab7b4be36cb8c44e361ccfc25cf757c15d1
Vinay Pandya <005vinay@gmail.com>
2018-08-11 15:09:48 +0530

    78. Setting up a 404

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -28,14 +28,21 @@ const HelpPage = () => (
   </div>
 );
 
+const NotFoundPage = () => (
+  <div>
+    404!
+  </div>
+);
+
 const routes = (
   <Router>
-    <div>
+    <Switch>
       <Route path="/create" component={AddExpensePage} />
       <Route path="/edit" component={EditExpensePage} />
       <Route path="/help" component={HelpPage} />
       <Route exact={true} path="/" component={ExpenseDashboardPage} />
-    </div>
+      <Route component={NotFoundPage}/>
+    </Switch>
   </Router>
 );
 
