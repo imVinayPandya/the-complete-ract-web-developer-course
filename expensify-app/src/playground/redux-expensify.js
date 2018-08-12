001f1263fc1ad111625efa00f25cc7fc1ed2135c
Vinay Pandya <005vinay@gmail.com>
2018-08-12 13:56:11 +0530

    92. Working with Multiple Reducers

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -1,20 +1,75 @@
 import { createStore, combineReducers } from 'redux';
+
+//
+// Expenses reducer
+//
+// ADD_EXPRENSE
+// EDIT_EXPRENSE
+// REMOVE_EXPRENSE
+
+const expensesReducerDefaultValue = [];
+const expensesReducer = (state = expensesReducerDefaultValue, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+//
+// Filters reducer
+//
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+const filtersReducerDefaultValue = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined
+};
+const filtersReducer = (state = filtersReducerDefaultValue, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+// Store creation
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
+);
+
+console.log(store.getState());
+
+//#region reducer rules and some info
 // Reducer : rules for reducer
 // 1. Reducer are pure function
 // 2. Never change state or action
 
-const demoState = {
-  expenses: [{
-    id: '123',
-    description: 'January rent',
-    note: 'This was the final payment for address',
-    amount: 54500,
-    createdAt: 0
-  }],
-  filters: {
-    text: 'rent',
-    sortBy: 'amount',
-    startDate: undefined,
-    endDate: undefined
-  }
-};
+// by looking below store, it has main 2 properties
+// 1. exprenses
+// 2. filters
+// based on that we can create two reducers
+
+// const demoState = {
+//   expenses: [{
+//     id: '123',
+//     description: 'January rent',
+//     note: 'This was the final payment for address',
+//     amount: 54500,
+//     createdAt: 0
+//   }],
+//   filters: {
+//     text: 'rent',
+//     sortBy: 'amount', // date or amount
+//     startDate: undefined,
+//     endDate: undefined
+//   }
+// };
+//#endregion
